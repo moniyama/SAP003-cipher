7590ef4a7a92099c2d31bfe52fe7b88cb9593cb8
moniyama <monicasayama@gmail.com>
2019-08-19 23:50:48 -0300

    utilizado loop for() ao inves de do() while()

--- src/cipher.js
+++ src/cipher.js
@@ -6,39 +6,33 @@ window.cipher = {
 function encode(){
   let offset = Number(document.getElementById('offset').value);
   let message = String(document.getElementById('text').value);
-  let length = message.length;
-  let arr = []; // cod ASCII da mensagem
-  let arrCifra = [] //mensagem cifrada transformada em string
-  let i=0;
+  let arr = [];          // cod ASCII da mensagem
+  let arrCifra = []     //mensagem cifrada transformada em string
 
-    do{
+
+    for(let i=0, length = message.length;i<length;i++){
       arr.push(((message.charCodeAt(i) - 65 + offset)%26)+65)
       arrCifra.push(String.fromCharCode(arr[i]))
-      i++}
-    while(i<length);
+      }
 
     document.getElementById('result').innerHTML =
     `<p> Codigo: ${arrCifra.join("")} </p>`
-    console.log(arr)
-    console.log(arrCifra)
+
   }
 
 function decode(){
   let offset = Number(document.getElementById('offset').value);
   let message = String(document.getElementById('text').value);
-  let length = message.length;
-  let arr = []; // cod ASCII da mensagem
-  let arrCifra = [] //mensagem cifrada transformada em string
+  let arr = [];       // cod ASCII da mensagem
+  let arrCifra = []   //mensagem cifrada transformada em string
   let i=0;
 
-    do{
-      arr.push(((message.charCodeAt(i) + 65 - offset) % 26)+65)
-      arrCifra.push(String.fromCharCode(arr[i]))
-      i++}
-    while(i<length);
+  for(let i=0, length = message.length;i<length;i++){
+    arr.push(((message.charCodeAt(i) + 65 - offset)%26)+65)
+    arrCifra.push(String.fromCharCode(arr[i]))
+    }
 
     document.getElementById('result').innerHTML =
     `<p> Decifrado: ${arrCifra.join("")} </p>`
-    console.log(arr)
-    console.log(arrCifra)
+
 }
