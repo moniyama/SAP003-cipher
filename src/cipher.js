0dda888fa8e4c2e32522bd00afbd4058ec0fead0
moniyama <monicasayama@gmail.com>
2019-08-19 23:02:54 -0300

    adicionado a função decode

--- src/cipher.js
+++ src/cipher.js
@@ -20,7 +20,19 @@ function encode(){
     console.log(arrCifra.join(""))
   }
 
-
 function decode(){
-  console.log('decode')
+  let offset = Number(document.getElementById('offset').value);
+  let message = String(document.getElementById('text').value);
+  let length = message.length;
+  let arr = []; // cod ASCII da mensagem
+  let arrCifra = [] //mensagem cifrada transformada em string
+  let i=0;
+
+    do{
+      arr.push(((message.charCodeAt(i) - 65 - offset)%26)+65)
+      arrCifra.push(String.fromCharCode(arr[i]))
+      i++}
+    while(i<length);
+
+    console.log(arrCifra.join(""))
 }
