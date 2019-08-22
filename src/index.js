87b8f872e5b0c964d9db36f1e314ee7e0aa97d15
moniyama <monicasayama@gmail.com>
2019-08-22 15:12:35 -0300

    ajuste

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-function btnEncode() {
+document.getElementById("code").onclick = function btnEncode() {
+  event.preventDefault();
+
   let offset = Number(document.getElementById("offset").value);
   let message = String(document.getElementById("text").value);
 
@@ -6,11 +8,13 @@ function btnEncode() {
     alert("Preencha os campos para prosseguir!");
   } else {
     document.getElementById("result").innerHTML =
-    `<p> Envie o Código:</p><p = id='textResult'> ${window.cipher.encode(offset, message)} </p>`;
+    `<p> Envie o Código:</p><textarea disabled id='textResult'>${window.cipher.encode(offset, message)}</textarea>
+    <p> Ah, não esqueça de enviar a chave para seu amigo: ${offset} </p>`;
   }
-}
+};
 
-function btnDecode() {
+document.getElementById("decode").onclick = function btnDecode() {
+  event.preventDefault();
   let offset = Number(document.getElementById("offset").value);
   let message = String(document.getElementById("text").value);
 
@@ -18,6 +22,6 @@ function btnDecode() {
     alert("Preencha os campos para prosseguir!");
   } else {
     document.getElementById("result").innerHTML =
-    `<p> Mensagem Decifrada: </p><p = id='textResult'> ${window.cipher.decode(offset, message)} </p>`;
+    `<p> Mensagem Decifrada: </p><textarea disabled id='textResult'>${window.cipher.decode(offset, message)}</textarea></p>`;
   }
-}
+};
