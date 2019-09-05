4ffaa2c0bac2e502252d58577a31fb6c0c5fd0d0
moniyama <monicasayama@gmail.com>
2019-09-05 19:42:30 -0300

    sugestão dada em feedback - CSS não está alinhando ao lado (flex)

--- src/cipher.js
+++ src/cipher.js
@@ -4,41 +4,41 @@ window.cipher = {
 };
 
 function encode (offset, message) {
-  const arr = [];          // array dos cod ASCII da mensagem
-  const arrCifra = [];     //array com as letras da mensagem cifrada
-  for (let i=0, length = message.length;i<length;i++) {
-    if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {   // se ASCII é das letras maiusculas
-      arr.push(((message.charCodeAt(i) - 65 + offset)%26)+65);    // cifrar
-      arrCifra.push(String.fromCharCode(arr[i]));
+  let arr;          // array dos cod ASCII da mensagem
+  let arrCifra = "";     //array com as letras da mensagem cifrada
+  for (let letter of message) {
+    let letterCode = letter.charCodeAt(0)
+    if (letterCode >= 65 && letterCode <= 90) {   // se ASCII é das letras maiusculas
+      arr = (65 + ((letterCode - 65 + offset) % 26));
+      arrCifra += String.fromCharCode(arr);
     } else {
-      if (message.charCodeAt(i) >= 97 && message.charCodeAt(i) <= 122) {   // se ASCII é das letras minuscula
-        arr.push(((message.charCodeAt(i) - 97 + offset)%26)+97);    // cifrar
-        arrCifra.push(String.fromCharCode(arr[i]));
+      if (letterCode >= 97 && letterCode <= 122) {   // se ASCII é das letras minuscula
+        arr = ((letterCode - 97 + offset)%26)+97;    // cifrar
+        arrCifra += String.fromCharCode(arr);
       } else {
-        arr.push(message.charCodeAt(i));         // coloca na array o mesmo codigo ASCII
-        arrCifra.push(String.fromCharCode(arr[i]));   // a string não sofre alteração
+        arrCifra += String.fromCharCode(letterCode);   // a string não sofre alteração
+        }
       }
-    }
   }
-  return arrCifra.join("");
+  return arrCifra;
 }
 
 function decode (offset, message) {
-  const arr = [];          // array dos cod ASCII da mensagem
-  const arrCifra = [];    //array com as letras da mensagem cifrada
-  for (let i=0, length = message.length;i<length;i++) {
-    if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {   // se ASCII é das letras maiusculas
-      arr.push(((message.charCodeAt(i) + 65 - offset)%26)+65);    // decifrar
-      arrCifra.push(String.fromCharCode(arr[i]));
+  let arr;
+  let arrCifra = "";
+  for (let letter of message) {
+    let letterCode = letter.charCodeAt(0)
+    if (letterCode >= 65 && letterCode <= 90) {   // se ASCII é das letras maiusculas
+      arr = ((letterCode + 65 - offset)%26)+65;    // decifrar
+      arrCifra += String.fromCharCode(arr);
     } else {
-      if (message.charCodeAt(i) >= 97 && message.charCodeAt(i) <= 122) {   // se ASCII é das letras minuscula
-        arr.push(((message.charCodeAt(i) + 111 - offset)%26)+97);    // decifrar
-        arrCifra.push(String.fromCharCode(arr[i]));
+      if (letterCode >= 97 && letterCode <= 122) {   // se ASCII é das letras minuscula
+        arr = ((message.charCodeAt(i) + 111 - offset)%26)+97;    // decifrar
+        arrCifra = String.fromCharCode(arr);
       } else {
-        arr.push(message.charCodeAt(i));         // coloca na array o mesmo codigo ASCII
-        arrCifra.push(String.fromCharCode(arr[i]));   // a string não sofre alteração
+        arrCifra = String.fromCharCode(letterCode);   // a string não sofre alteração
       }
     }
   }
-  return arrCifra.join("");
+  return arrCifra;
 }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
-document.getElementById("code").onclick = function btnEncode() {
+const offset = document.getElementById("offset");
+const message = document.getElementById("text");
+
+document.getElementById("code").addEventListener("click", btnEncode)
+document.getElementById("decode").addEventListener("click", btnDecode)
+
+function btnEncode() {
   event.preventDefault();
+  validation();
+  document.getElementById("result").innerHTML =
+  `<p> Envie o Código:</p><textarea disabled id='textResult'>${window.cipher.encode(+offset.value, message.value)}</textarea>
+  <p> Ah, não esqueça de enviar a chave para seu amigo: ${offset.value} </p>`;
+}
 
-  let offset = Number(document.getElementById("offset").value);
-  let message = String(document.getElementById("text").value);
-
-  if (offset === 0 || message === "" ) {
-    alert("Preencha os campos para prosseguir!");
-  } else {
-    document.getElementById("result").innerHTML =
-    `<p> Envie o Código:</p><textarea disabled id='textResult'>${window.cipher.encode(offset, message)}</textarea>
-    <p> Ah, não esqueça de enviar a chave para seu amigo: ${offset} </p>`;
-  }
+function btnDecode() {
+  event.preventDefault();
+  validation();
+  document.getElementById("result").innerHTML =
+  `<p> Mensagem Decifrada: </p><textarea disabled id='textResult'>${window.cipher.decode(+offset.value, message.value)}</textarea>
+  <p> Envie uma mensagem de volta! </p>`;
 };
 
-document.getElementById("decode").onclick = function btnDecode() {
-  event.preventDefault();
-  let offset = Number(document.getElementById("offset").value);
-  let message = String(document.getElementById("text").value);
-
-  if (offset === 0 || message === "" ) {
+function validation () {
+  if (+offset.value === 0 || message.value === "" ) {
     alert("Preencha os campos para prosseguir!");
-  } else {
-    document.getElementById("result").innerHTML =
-    `<p> Mensagem Decifrada: </p><textarea disabled id='textResult'>${window.cipher.decode(offset, message)}</textarea>
-    <p> Envie uma mensagem de volta! </p>`;
   }
-};
+}
