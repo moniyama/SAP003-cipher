28b0d67e3e76313c0a077e9d524b569a0ae884dc
moniyama <monicasayama@gmail.com>
2019-10-02 01:11:06 -0300

    revisão do html, index.js e css

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
 const offset = document.getElementById("offset");
 const message = document.getElementById("text");
+const resultado = document.getElementById("result");
+const buttons = document.querySelectorAll(".my-btn");
 
-document.getElementById("code").addEventListener("click", btnEncode)
-document.getElementById("decode").addEventListener("click", btnDecode)
-
-function btnEncode() {
-  event.preventDefault();
-  validation();
-  document.getElementById("result").innerHTML =
-  `<p> Envie o Código:</p><textarea disabled id='textResult'>${window.cipher.encode(+offset.value, message.value)}</textarea>
-  <p> Ah, não esqueça de enviar a chave para seu amigo: ${offset.value} </p>`;
+for (let btn of buttons) {
+  btn.addEventListener("click", decide);
 }
 
-function btnDecode() {
-  event.preventDefault();
-  validation();
-  document.getElementById("result").innerHTML =
-  `<p> Mensagem Decifrada: </p><textarea disabled id='textResult'>${window.cipher.decode(+offset.value, message.value)}</textarea>
-  <p> Envie uma mensagem de volta! </p>`;
-};
+function decide() {
+  displayWarning();
+  let id = this.id;
+  if (id == "code") {
+    resultado.innerHTML =
+      `<p> Envie o Código:</p><textarea disabled id='textResult'>${window.cipher.encode(+offset.value, message.value)}</textarea>
+    <p> Ah, não esqueça de enviar a chave para seu amigo: ${offset.value} </p>`;
+  } else {
+    resultado.innerHTML =
+      `<p> Mensagem Decifrada: </p><textarea disabled id='textResult'>${window.cipher.decode(+offset.value, message.value)}</textarea>
+    <p> Envie uma mensagem de volta! </p>`;
+  }
+}
 
-function validation () {
-  if (+offset.value === 0 || message.value === "" ) {
+function displayWarning(event) {
+  if (+offset.value === 0 || message.value === "") {
     alert("Preencha os campos para prosseguir!");
+    event.preventDefault();
   }
 }
