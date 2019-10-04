a88666e547dfe31863be73f94118b4f3d99af89d
moniyama <monicasayama@gmail.com>
2019-10-04 09:58:48 -0300

    add menu, mudança de abas no click

--- src/index.js
+++ src/index.js
@@ -1,26 +1,43 @@
-const offset = document.getElementById("offset");
-const message = document.getElementById("text");
-const resultado = document.getElementById("result");
-const buttons = document.querySelectorAll(".my-btn");
+const nav = document.querySelectorAll(".menu");
+for (let li of nav) {
+  li.addEventListener("click", showSection) 
+}
 
+const sections = document.querySelectorAll(".seção")
+function showSection () {
+  hideSection();
+  const menuEscolhido = document.getElementById(this.dataset.target)
+  menuEscolhido.style.display = "block";
+}
+function hideSection() {
+  for (let aba of sections) {
+    aba.style.display = "none"; 
+  }
+}
+
+const buttons = document.querySelectorAll(".my-btn");
 for (let btn of buttons) {
   btn.addEventListener("click", decide);
 }
 
+const offset = document.getElementById("offset");
+const message = document.getElementById("text");
+
 function decide() {
   displayWarning();
-  let id = this.id;
+  
+  const resultado = document.getElementById("result");
+  const id = this.id;
   if (id == "code") {
     resultado.innerHTML =
-      `<p> Envie o Código:</p><textarea disabled id='textResult'>${window.cipher.encode(+offset.value, message.value)}</textarea>
+    `<p> Envie o Código:</p><textarea disabled id='textResult'>${window.cipher.encode(+offset.value, message.value)}</textarea>
     <p> Ah, não esqueça de enviar a chave para seu amigo: ${offset.value} </p>`;
   } else {
     resultado.innerHTML =
-      `<p> Mensagem Decifrada: </p><textarea disabled id='textResult'>${window.cipher.decode(+offset.value, message.value)}</textarea>
+    `<p> Mensagem Decifrada: </p><textarea disabled id='textResult'>${window.cipher.decode(+offset.value, message.value)}</textarea>
     <p> Envie uma mensagem de volta! </p>`;
   }
 }
-
 function displayWarning(event) {
   if (+offset.value === 0 || message.value === "") {
     alert("Preencha os campos para prosseguir!");
