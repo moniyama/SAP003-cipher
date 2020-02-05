8a0f9e6a327797bf90c4828731f86b9409d4e7b6
Monica Yamazaki <monicasayama@gmail.com>
2020-02-04 21:12:14 -0300

    decode fixed

--- src/cipher.js
+++ src/cipher.js
@@ -3,40 +3,40 @@ window.cipher = {
   decode: decode
 };
 
-function encode (offset, message) {
-  let arr;          // array dos cod ASCII da mensagem
-  let arrCifra = "";     //array com as letras da mensagem cifrada
+function encode(offset, message) {
+  let arr; 
+  let arrCifra = "";
   for (let letter of message) {
-    let letterCode = letter.charCodeAt(0)
-    if (letterCode >= 65 && letterCode <= 90) {   // se ASCII é das letras maiusculas
-      arr = (65 + ((letterCode - 65 + offset) % 26));
+    let letterCode = letter.charCodeAt(0);
+    if (letterCode >= 65 && letterCode <= 90) {
+      arr = 65 + ((letterCode - 65 + offset) % 26);
       arrCifra += String.fromCharCode(arr);
     } else {
-      if (letterCode >= 97 && letterCode <= 122) {   // se ASCII é das letras minuscula
-        arr = ((letterCode - 97 + offset)%26)+97;    // cifrar
+      if (letterCode >= 97 && letterCode <= 122) {
+        arr = ((letterCode - 97 + offset) % 26) + 97; 
         arrCifra += String.fromCharCode(arr);
       } else {
-        arrCifra += String.fromCharCode(letterCode);   // a string não sofre alteração
-        }
+        arrCifra += String.fromCharCode(letterCode);
       }
+    }
   }
   return arrCifra;
 }
 
-function decode (offset, message) {
+function decode(offset, message) {
   let arr;
   let arrCifra = "";
   for (let letter of message) {
-    let letterCode = letter.charCodeAt(0)
-    if (letterCode >= 65 && letterCode <= 90) {   // se ASCII é das letras maiusculas
-      arr = ((letterCode + 65 - offset)%26)+65;    // decifrar
+    let letterCode = letter.charCodeAt(0);
+    if (letterCode >= 65 && letterCode <= 90) {
+      arr = ((letterCode + 65 - offset) % 26) + 65;
       arrCifra += String.fromCharCode(arr);
     } else {
-      if (letterCode >= 97 && letterCode <= 122) {   // se ASCII é das letras minuscula
-        arr = ((message.charCodeAt(i) + 111 - offset)%26)+97;    // decifrar
-        arrCifra = String.fromCharCode(arr);
+      if (letterCode >= 97 && letterCode <= 122) {
+        arr = ((letterCode + 111 - offset) % 26) + 97;
+        arrCifra += String.fromCharCode(arr);
       } else {
-        arrCifra = String.fromCharCode(letterCode);   // a string não sofre alteração
+        arrCifra += String.fromCharCode(letterCode);
       }
     }
   }
